b89eabbedc12030bfc84f77b5d506c5226e88d16
NileshGoswami <nilesh.goswami@leftrightmind.com>
2019-01-02 15:50:06 +0530

    Fist commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+		Debug.Log("I am Here");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+		RaycastHit hit;
+
+		if (Input.GetKeyDown("space"))
+		{
+			if (Physics.Raycast(transform.position, transform.forward, out hit))
+			{
+
+				if (hit.transform.GetComponent<mole>() != null) {
+					Debug.Log("Hit a Mole");
+				}
+			}
+		}
+
+	}
+}
